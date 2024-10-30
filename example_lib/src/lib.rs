480b140c18e396b2331e4e4f0d2bcaec795aafa0
xshady <54737754+xxshady@users.noreply.github.com>
2024-10-31 00:10:15 +0300

    to investigate

--- example_lib/src/lib.rs
+++ example_lib/src/lib.rs
@@ -1,14 +1,23 @@
+use std::{collections::HashMap, hash::RandomState, sync::Mutex};
+
 #[unsafe(no_mangle)]
 pub static mut MAIN_CALLED: bool = false;
+
+#[unsafe(no_mangle)]
+pub static mut PRINT: unsafe extern "C" fn(&str) = print_placeholder;
+
+unsafe extern "C" fn print_placeholder(_: &str) {}
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn main() {
     MAIN_CALLED = true;
 
-    // prevents unloading     
-    std::thread::scope(|s| {
-        s.spawn(|| {});
-    });
-    // works fine
-    // std::thread::spawn(|| {}).join().unwrap();
+    PRINT("5");
+
+    static ALLOCS_CACHE: Mutex<Option<HashMap<i32, i32>>> = Mutex::new(None);
+
+    ALLOCS_CACHE
+        .lock()
+        .unwrap()
+        .replace(HashMap::with_capacity(20_000));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use libloading::os::unix::{RTLD_LAZY, RTLD_LOCAL, Library};
+use std::{fs, io::stdin, path::PathBuf, time::Duration};
+
+use libloading::os::unix::{Library, RTLD_LAZY, RTLD_LOCAL};
 
 fn main() {
-    load_and_unload();
-    println!("----------------------------");
-    load_and_unload();
+    loop {
+        load_and_unload();
+        println!("----------------------------");
+
+        let mut message = String::new();
+        stdin().read_line(&mut message).unwrap();
+        if message == "q\n" {
+            return;
+        }
+    }
+    // load_and_unload();
 }
 
 fn load_and_unload() {
@@ -15,27 +25,36 @@ fn load_and_unload() {
         };
 
         let lib_path = format!("target/{directory}/libexample_lib.so");
-        dbg!(&lib_path);
+        dbg!(&lib_path, &PathBuf::from(&lib_path).parent());
 
-        let lib = Library::open(
-            Some(lib_path),
-            RTLD_LAZY | RTLD_LOCAL,
-        )
-        .unwrap();
+        let copy_path = PathBuf::from(&lib_path)
+            .parent()
+            .unwrap()
+            .join("kgjkfjgfgkjhjekw.so");
+
+        fs::copy(&lib_path, &copy_path).unwrap();
+
+        let lib = Library::open(Some(copy_path), RTLD_LAZY | RTLD_LOCAL).unwrap();
 
         let main_called: *mut bool = *lib.get(b"MAIN_CALLED\0").unwrap();
         if *main_called {
             panic!("library is not unloaded");
         }
 
-        type MainFn =
-            unsafe extern "C" fn();
+        let print_fn: *mut unsafe extern "C" fn(&str) = *lib.get(b"PRINT\0").unwrap();
+
+        *print_fn = print_impl;
+
+        unsafe extern "C" fn print_impl(msg: &str) {
+            println!("dylib: {msg}");
+        }
+
+        type MainFn = unsafe extern "C" fn();
 
         let main_fn: MainFn = *lib.get(b"main\0").unwrap();
         main_fn();
 
-        // no difference
         lib.close().unwrap();
-        // drop(lib);
+        // std::mem::forget(lib);
     }
 }
